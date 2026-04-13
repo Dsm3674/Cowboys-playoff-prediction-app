19e6b789c93ccfe12643a7f349674be9c2df9ad5
Divyanshu <divyanshusomasekhar1@gmail.com>
2026-04-13 18:08:15 -0500

    Create GlobalLoader.jsx

---
+++ frontend/src/components/GlobalLoader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function GlobalLoader() {
+  return (
+    <div className="app-loading">
+      <div className="loader-box">
+        <div className="loader-title">LONESTAR ANALYTICS</div>
+        <div className="loader-bar"></div>
+      </div>
+    </div>
+  );
+}
